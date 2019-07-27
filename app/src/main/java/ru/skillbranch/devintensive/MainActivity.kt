fb36a4653ff2667fc2ea6e12842a86f36bd4b3a2
Alex-Bur <51814739+Alex-Bur@users.noreply.github.com>
2019-07-27 17:59:56 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,4 +1,157 @@
 package ru.skillbranch.devintensive
 
-import android.support.v4.*
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var benderImage: ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBtn: ImageView
+
+    lateinit var benderObj: Bender
+
+    /**
+     * Вызывается при первом создании или перезапуске Фсешмшеню
+     * здесь задается внешний вид активности (UI) через метод SetContentView().
+     * инициализируются представления
+     * предстваления связываются с необходимыми данными и ресурсами
+     * связываются данные со списками
+     *
+     * Этот метод также предоставляет Bundle, содержащий ранее сохраненное
+     * состояние Activity, если оно было
+     *
+     * Всегда сопровождается вызовом onStart().
+     */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        benderImage = findViewById(R.id.iv_bender) as ImageView
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBtn =iv_send
+
+        val status = savedInstanceState?.getString("STATUS") ?: Bender.Status.NORMAL.name
+        val question = savedInstanceState?.getString("QUESTION") ?: Bender.Question.NAME.name
+
+        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+
+        Log.d("M_MainActivity","OnCreate $status $question")
+        val (r,g,b) = benderObj.status.color
+        benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
+
+        textTxt.text = benderObj.askQuestion()
+        sendBtn.setOnClickListener(this)
+    }
+
+
+    /**
+     * Если Activity возвращается в приоритетный режим после вызова onStop(),
+     * то в этом случае вызывается метод onRestart().
+     * Т.е. вызывается после того, как Activity была остановлена и снова была запущена пользователем.
+     * Всегда сопровождается вызовом метода onStart().
+     *
+     * Используется для специальных действий, которые должны выполняться только при повторном запуске Activity
+     */
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("M_MainActivity","OnRestart")
+    }
+
+    /**
+     * При вызове onStart() окно еще не видно пользователю, но вскоре будет видно.
+     * Вызывается непосредственно перед тем, как активность становится видимой пользователю.
+     *
+     * Четние из БД
+     * Запуск сложной анимации
+     * Запуск потоков, отслеживаине показаний датчиков, запросов к GPS, сервисов и других процессов,
+     * которые нужны исключительно для обновления пользовательского интерфейча
+     *
+     * Затем следует onResume(), если Activity выходит на передний план.
+     */
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("M_MainActivity","OnStart")
+    }
+
+
+    /**
+     * Вызывается когда Activity начнет взаимодействовать с пользователем.
+     *
+     * Запуск воспроизведения анимации, аудио и видео
+     * регистрация любых BroadcastReceiver или других процессов, которые вы освободили/приостановили в onPause()
+     * выполнение любых других инициализаций, которые должны происходить, когда Activity вновь активна (камера).
+     *
+     * Тут должен быть максимально легкий и быстрый код, чтобы приложение оставалось отзывчивым
+     */
+    override fun onResume() {
+        super.onResume()
+        Log.d("M_MainActivity","OnResume")
+    }
+
+        override fun onPause() {
+        super.onPause()
+        Log.d("M_MainActivity","onPause")
+    }
+
+
+    /**
+     * Метод onStop() вызывается, когда Activity становится невидимым для пользователя.
+     * Это может произойти при ее уничтожении, или если была запущена другая Activity (существующая или новая),
+     * перекрывшая окно текущей Activity.
+     *
+     * запись в БД
+     * приостановка сложной анимации
+     * приостановка потоков, отслеживания показаний датчиков, запросов к GPS, таймеров, сервисов и других процессов,
+     * которые нужны исключительно для обновления ползовательского интерфейса
+     *
+     * Не вызывается при вызове метода finish() у Activity
+     */
+    override fun onStop() {
+        super.onStop()
+        Log.d("M_MainActivity","onStop")
+    }
+
+
+    /**
+     * Метод вызывается по окончании работы Activity, при вызове метода finish() или в случае,
+     * когда система уничтожает этот экземпляр активности для освобождения ресурсов
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("M_MainActivity","onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putString("STATUS", benderObj.status.name)
+        outState?.putString("QUESTION", benderObj.question.name)
+        Log.d("M_MainActivity","onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.iv_send){
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            messageEt.setText("")
+            val (r,g,b) = color
+            benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+        }
+    }
+}
+
