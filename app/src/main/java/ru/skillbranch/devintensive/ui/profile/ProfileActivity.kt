acdba43386f1b21b11f60255e76a514e5dc42060
Alex-Bur <51814739+Alex-Bur@users.noreply.github.com>
2019-07-29 08:35:43 +0300

    hometask_4 complete

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,26 +1,19 @@
-package ru.skillbranch.devintensive
+package ru.skillbranch.devintensive.ui.profile
 
-import android.graphics.Color
-import android.graphics.PorterDuff
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import ru.skillbranch.devintensive.models.Bender
+import ru.skillbranch.devintensive.R.layout.activity_profile
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class ProfileActivity : AppCompatActivity(), View.OnClickListener {
 
-    lateinit var benderImage: ImageView
+/*    lateinit var benderImage: ImageView
     lateinit var textTxt: TextView
     lateinit var messageEt: EditText
     lateinit var sendBtn: ImageView
 
-    lateinit var benderObj: Bender
+    lateinit var benderObj: Bender*/
 
     /**
      * Вызывается при первом создании или перезапуске Фсешмшеню
@@ -37,10 +30,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_profile)
 
 //        benderImage = findViewById(R.id.iv_bender) as ImageView
-        benderImage = iv_bender
+/*        benderImage = iv_bender
         textTxt = tv_text
         messageEt = et_message
         sendBtn =iv_send
@@ -55,7 +48,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
 
         textTxt.text = benderObj.askQuestion()
-        sendBtn.setOnClickListener(this)
+        sendBtn.setOnClickListener(this)*/
     }
 
 
@@ -139,19 +132,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-        outState?.putString("STATUS", benderObj.status.name)
+/*        outState?.putString("STATUS", benderObj.status.name)
         outState?.putString("QUESTION", benderObj.question.name)
-        Log.d("M_MainActivity","onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")
+        Log.d("M_MainActivity","onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")*/
     }
 
     override fun onClick(v: View?) {
-        if (v?.id == R.id.iv_send){
+/*        if (v?.id == R.id.iv_send){
             val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
             messageEt.setText("")
             val (r,g,b) = color
             benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
             textTxt.text = phrase
-        }
+        }*/
     }
 }
 
